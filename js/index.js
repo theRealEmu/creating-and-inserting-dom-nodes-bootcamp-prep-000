ad4c19747d8c825d1716cd54cec3e1aaf23673ea
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-21 12:47:51 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+element.innerText = "Hello Emu";
+eleme
