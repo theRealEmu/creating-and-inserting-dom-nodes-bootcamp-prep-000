3bf7c7f42ef4644da58fee9983d1b84d793e429c
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-21 12:55:26 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ for( let i = 0; i < 5; i += 1) {
 
 ul.style.textAlign = 'left';
 document.querySelector('#emu').appendChild(ul);
-document.querySelector('#emu').removeChild();
+document.querySelector('#emu').removeChild(ul.querySelector('li:nth-child(2)'));
 
 
 
